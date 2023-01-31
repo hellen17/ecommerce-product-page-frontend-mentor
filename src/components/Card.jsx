013175ec7270a98db4ffa7faf00c7f11209e754c
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-01-31 22:38:59 +0300

    make slider images responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,21 @@
 import { Image } from 'antd';
 import { FaChevronLeft, FaChevronRight  } from "react-icons/fa";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Card({image, products}){
     const [currentIndex, setCurrentIndex] = useState(0);
 
+    const [isDesktop, setIsDesktop] = useState(false)
+
+    console.log('isDesktop', isDesktop)
+
+    useEffect(() => {
+      const handleResize = () => setIsDesktop(window.innerWidth > 768)
+      window.addEventListener('resize', handleResize)
+      return () => window.removeEventListener('resize', handleResize)
+     
+    }, [])
+    
     const nextSlide = () => {
       if (currentIndex === products.length - 1) {
         setCurrentIndex(0)
@@ -25,10 +36,11 @@ export default function Card({image, products}){
     const mainImage = products[currentIndex].mainImage
     console.log('mainImage', mainImage)
 
-    
+
+
     return(
       <div className='relative'>
-       <img className='lg:rounded-lg' src={mainImage} alt="product images" width={450} />
+       <img className='lg:rounded-lg' src={ isDesktop ? image : mainImage } alt="product images" width={450} />
         
       <ul className='lg:hidden'>
       <li onClick={previousSlide} className='absolute rounded-full bg-slate-100 p-3 left-4 top-1/2'> <FaChevronLeft/> </li>
