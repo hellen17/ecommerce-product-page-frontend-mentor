f7bf247f5b0852879ad0b3a0d6342322ac15f38f
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-02-02 11:43:52 +0300

    install tailwindcss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Badge, Popover } from 'antd';
 import { AiFillDelete } from "react-icons/ai";
 import logo from '/images/logo.svg'
 import avatar from '/images/image-avatar.png'
+import hamburger from '/images/icon-menu.svg'
+import close from '/images/icon-close.svg'
 // const { SolidButton } = require('./Button');
 
 export default function Navbar({count, cart}) {
+  const [isOpen, setIsOpen] = useState(false);
+
   const text = <span> <b>Cart</b> </span>;
   console.log('cart', cart[0].name)
   const {  name, discount_price } = cart[0];
@@ -20,15 +25,20 @@ export default function Navbar({count, cart}) {
       </ul>
       <AiFillDelete className="text-red-500 text-xl"/>
       </div>
-      <button className='bg-orange-500 hover:bg-orange-400 text-white shadow py-2 px-4 mt-3 rounded-lg justify-center w-full w-full'>Checkout</button>
+      <button className='bg-orange-500 hover:bg-orange-400 text-white shadow py-2 px-4 mt-3 rounded-lg justify-center w-full'>Checkout</button>
 
     </div>
   );
   return (
-    <header className="flex justify-between items-center py-3 border-b-2">
+    <header className="flex justify-between items-center p-3 border-b-2">
     <div className="flex items-center justify-start gap-4">
-      <img src={logo} alt="logo" />
-      <nav className="flex hidden items-center justify-start gap-4">
+      <ul className="flex gap-3 items-center">
+        <li onClick={() => setIsOpen(true)} className="lg:hidden"> <button><img src={hamburger} alt="menu" /></button> </li>
+        {isOpen && <li onClick={() => setIsOpen(false)} className="lg:hidden close"> <button><img src={close} alt="close" /></button> </li>}
+        <li><img src={logo} alt="logo" /></li>
+      </ul>
+   
+      <nav className=  {isOpen ? 'open' : 'flex flex-row items-start gap-3'} >
         <a href="/">Collections</a>
         <a href="#">Men</a>
         <a href="#">Women</a>
